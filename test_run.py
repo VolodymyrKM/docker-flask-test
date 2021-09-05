64fa8edee649c75e24c339377b275402ba41c542
VolodymyrKM <kolomiecnew@gmail.com>
2021-09-05 17:34:30 +0300

    update and add travis3

--- test_run.py
+++ test_run.py
@@ -1,7 +1,7 @@
 import requests
-from http import HTTPStatus
+
 
 
 def test_index():
     response = requests.get("http://localhost:5000")
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == 200

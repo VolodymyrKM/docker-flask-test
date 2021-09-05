32484abbd9c070f87247bb13ced21a1cf2cb81ed
VolodymyrKM <kolomiecnew@gmail.com>
2021-09-05 17:52:34 +0300

    updatet travis4

--- test_run.py
+++ test_run.py
@@ -1,7 +1,20 @@
-import requests
+import pytest
+from run import app
 
 
+@pytest.fixture
+def test_cli():
+    return app.test_client()
 
-def test_index():
-    response = requests.get("http://localhost:5000")
+
+def test_index(test_cli):
+    response = test_cli.get("/")
     assert response.status_code == 200
+
+
+def test_index_response(test_cli):
+    response = test_cli.get("/")
+    assert response.data == b"<h1> Hello world 3 </h1>"
+
+
+
